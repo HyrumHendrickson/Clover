44f76eaa9f9bd40fe472ed6c2349e76f20104e3d
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-04-26 11:38:03 -0600

    added restart msg

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,3 +2,11 @@
 
 source backups/clover_installer.sh
 cd /usr/local/lib/clover
+
+echo ""
+echo ""
+echo "------------------------------------"
+echo "RESTSART CLOVER TO FINISH UPDATE"
+echo "------------------------------------"
+echo ""
+echo ""
