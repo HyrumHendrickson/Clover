ebb0ae4973eba7b93c3361c9eb195f4c192f580f
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-04-26 11:30:34 -0600

    updated installer

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,13 +2,3 @@
 
 source backups/clover_installer.sh
 cd /usr/local/lib/clover
-
-
-echo ""
-echo ""
-echo "-------------------------------------"
-echo "RESTART CLOVER FOR UPDATE TO FINISH"
-echo "run 'exit' command, then run 'clover'"
-echo "-------------------------------------"
-echo ""
-echo ""
