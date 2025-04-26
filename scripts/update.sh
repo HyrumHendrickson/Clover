1fc92f9d7f253a7dbc61f9264767be9755eb8252
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-04-26 10:49:10 -0600

    added more text

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
 source backups/clover_installer.sh
+echo ""
+echo ""
+echo "-------------------------------------"
 echo "RESTART CLOVER FOR UPDATE TO FINISH"
 echo "run 'exit' command, then run 'clover'"
+echo "-------------------------------------"
+echo ""
+echo ""
