339e36fdaf9c4d354fb49286844fb205cf8706f0
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-04-26 09:43:31 -0600

    first commit

---
+++ scripts/commands.sh
@@ -0,0 +1,55 @@
+run_command() {
+  local input="$1"
+
+  
+  read -a tokens <<< "$input"
+
+  case "${tokens[0]}" in
+  
+    help)
+      echo "List of commands:
+-help
+-test
+-save
+-list
+-start
+-clear
+-update
+-exit
+-quit"
+      ;;
+      
+    test)
+      echo "ok"
+      ;;
+      
+    save)
+      echo "${tokens[2]}" > data/env_vars/"${tokens[1]}"
+      echo "saved"
+      ;;
+
+    list)
+		for file in data/env_vars/*; do
+		  echo -n "$(basename "$file"): "
+		  cat "$file"
+		done
+		
+    ;;
+      
+    start)
+      cat assets/logo.txt
+      ;;
+
+    clear)
+      clear
+      ;;
+
+    update)
+      source scripts/update.sh
+      ;;
+      
+    *)
+      echo "Unknown command: $input"
+      ;;
+  esac
+}
