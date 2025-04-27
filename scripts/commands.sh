43e10c6e5957b8e6b926dcde405a2b56d1115bfa
Hyrum Hendrickson <94187900+HyrumHendrickson@users.noreply.github.com>
2025-04-27 11:59:49 -0600

    hid data folders

--- scripts/commands.sh
+++ scripts/commands.sh
@@ -25,16 +25,16 @@ run_command() {
       ;;
       
     save)
-      [ -d "$HOME/clover_files" ] || mkdir -p "$HOME/clover_files"
+      [ -d "$HOME/.clover_files" ] || mkdir -p "$HOME/.clover_files"
       echo "${tokens[2]}" > ~/clover_files/"${tokens[1]}"
       echo "saved"
       ;;
 
     list)
-        [ -d "$HOME/clover_files" ] || mkdir -p "$HOME/clover_files"
+        [ -d "$HOME/.clover_files" ] || mkdir -p "$HOME/.clover_files"
 
 
-        if [ "$(find $HOME/clover_files -mindepth 1 | head -n 1)" ]; then
+        if [ "$(find $HOME/.clover_files -mindepth 1 | head -n 1)" ]; then
             for file in $HOME/clover_files/*; do
                 echo -n "$(basename "$file"): "
             	cat "$file"
